02bcadaeb99415d224ecd1cda3e5e19747484143
Matusz27 <mateusz.wozniak2000@gmail.com>
2022-01-04 18:21:45 +0100

    starting working on the chart

--- src/app/services/steam-call.service.ts
+++ src/app/services/steam-call.service.ts
@@ -45,8 +45,8 @@ export class SteamCallService {
 
     let url = `http://localhost:4200/api/${id}/wishlistdata/`
 
-    for (let page = 0; page < 150; page++) {
-
+    for (let page = 0; page < 1; page++) {
+        //150
         let query = {'p': page, 'cc': this.selectedCountry}
 
         let steamCall = await this.http.get<any>(url, {params: query, responseType: "json"}).toPromise()
