63c059daf8dff2f3007161c7300f58fad20b5c1a
Matusz27 <mateusz.wozniak2000@gmail.com>
2022-01-10 21:58:21 +0100

    parsing json

--- functions/api/[[id]].ts
+++ functions/api/[[id]].ts
@@ -5,10 +5,11 @@
 
 export async function onRequestGet(context:any) {
     
-        //const params = context.params;
+        const params = context.params;
+        
         //let url = "https://store.steampowered.com/wishlist/profiles/"
         //params.forEach((para:string) => {
         //    url += para
         //});
-        return new Response('{text:"Response"}', {status: 200} )
+        return new Response(JSON.parse(params), {status: 200} )
 }
