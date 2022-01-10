70efd6abfc06a0203ce34b6726f252a50e67d5d8
Matusz27 <mateusz.wozniak2000@gmail.com>
2022-01-10 22:25:33 +0100

    testing url

--- functions/api/[[id]].ts
+++ functions/api/[[id]].ts
@@ -7,10 +7,10 @@ export async function onRequestGet(context:any) {
     
         const params = context.params;
         
-        //let url = "https://store.steampowered.com/wishlist/profiles/"
+        let url = "https://store.steampowered.com/wishlist/profiles/"
         //params.forEach((para:string) => {
         //    url += para
         //});
         
-        return new Response(params[0], {status: 200} )
+        return new Response(url, {status: 200} )
 }
