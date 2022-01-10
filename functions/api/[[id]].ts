c466df29851670252bc975cfd4f771dfe225d92e
Matusz27 <mateusz.wozniak2000@gmail.com>
2022-01-10 19:30:30 +0100

    new function

--- functions/api/[[id]].ts
+++ functions/api/[[id]].ts
@@ -2,14 +2,19 @@
 
 
 
+
 export async function onRequestGet(context:any) {
-    try {
+    
         const params = context.params;
-        let response = await fetch("https://store.steampowered.com/wishlist/profiles/" + params);
+        let url = "https://store.steampowered.com/wishlist/profiles/"
+        params.forEach((para:string) => {
+            url += para
+        });
+    try {
+        let response = await fetch(url);
         return new Response( await response.json())
     } catch (err) {
-        const json = '{"err":' + err + "}"
-        return new Response(JSON.parse(json))
+        return new Response("Error")
     }
 
 }
